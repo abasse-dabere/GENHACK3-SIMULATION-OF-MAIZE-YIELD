c13a592a3283fb237d40c3c6e55d8c29c9c41d70
michael-allouche <mjs.allouche@gmail.com>
2024-02-01 00:54:18 +0100

    new files for evaluation 2

--- model.py
+++ model.py
@@ -8,13 +8,12 @@
 # <!> *SAVE YOUR PARAMETERS IN THE parameters/ DICRECTORY* <!>
 #
 # See below an example of a generative model
-# Z |-> G_\theta(Z)
+# Z,x |-> G_\theta(Z,x)
 ############################################################################
-from tensorflow.keras.models import load_model
-import joblib
 
 # <!> DO NOT ADD ANY OTHER ARGUMENTS <!>
-def generative_model(noise):
+
+def generative_model(noise, scenario):
     """
     Generative model
 
@@ -22,20 +21,19 @@ def generative_model(noise):
     ----------
     noise : ndarray with shape (n_samples, n_dim=4)
         input noise of the generative model
+    scenario: ndarray with shape (n_samples, n_scenarios=9)
+        input categorical variable of the conditional generative model
     """
     # See below an example
     # ---------------------
-    latent_dim = 50
-    latent_variable = noise[:, :latent_dim]  # choose the appropriate latent dimension of your model
+    latent_variable = noise[:, ...]  # choose the appropriate latent dimension of your model
 
     # load your parameters or your model
     # <!> be sure that they are stored in the parameters/ directory <!>
-    model = load_model('parameters/g1_with_d3.h5')
-    scaler = joblib.load('parameters/output_scaler.pkl')
-    outputs = model(latent_variable)
-    outputs = scaler.inverse_transform(outputs)
+    model = ...
 
-    return outputs # G(Z)
+    return model(latent_variable) # G(Z)
+    # return model(latent_variable, scenario) # G(Z, x)
 
 
 
